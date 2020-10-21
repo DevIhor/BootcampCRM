def8563539479f5a6e58149c6916ae7f0dc7d6bb
IhorLutsiuk <ihor.l@coaxsoft.com>
2020-10-21 17:55:24 +0300

    Added News and NewsTag models

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+from apps.common.models import Slugged, Ownable
+
+
+class NewsTag(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        app_label = 'blog'
+        db_table = 'blog__news_tags'
+        verbose_name = 'NewsTag'
+        verbose_name_plural = 'NewsTags'
+
+
+class News(Slugged, Ownable):
+    title = models.CharField(max_length=255)
+    is_published = models.BooleanField(default=False)
+    author = models.CharField(max_length=255)
+    tags = models.ManyToManyField(to=NewsTag, blank=True, related_name='news')
+    content = models.TextField()
+
+    def autofill_author(self):
+        if not self.author:
+            self.author = f'{self.user.first_name} {self.user.last_name}'
+
+    def save(self, *args, **kwargs):
+        self.autofill_author()
+        super(News, self).save(*args, **kwargs)
+
+    class Meta:
+        app_label = 'blog'
+        db_table = 'blog__news'
+        verbose_name = 'News'
+        verbose_name_plural = 'News'
